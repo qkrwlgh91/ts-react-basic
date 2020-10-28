53adf15b6dc0415b781bcd6f9d19930ce452a545
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-10-28 14:15:26 +0900

    added todo tsx files for practicing regaring context api

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import MyForm from './MyForm';
 import CounterReducer from './CounterReducer';
 import ReducerSample from './ReducerSample';
 
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
 function App() {
   const onClick = (name: string) => {
     console.log(`${name} says hello`);
@@ -23,6 +26,10 @@ function App() {
       <MyForm onSubmit={onSubmit} />
       <CounterReducer />
       <ReducerSample />
+      <div>
+        <TodoForm/>
+        <TodoList/>
+      </div>
     </div>
     
   );
