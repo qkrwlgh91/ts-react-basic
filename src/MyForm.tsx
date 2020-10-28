bf2f65442b09efa84839b77fda6b8c1d622acaad
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-10-28 11:12:45 +0900

    added useRef ( input focus())

--- src/MyForm.tsx
+++ src/MyForm.tsx
@@ -1,10 +1,13 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 type MyFormProps = {
     onSubmit: (form: { name: string, description: string }) => void;
 }
 
+
+
 function MyForm({ onSubmit }: MyFormProps) {
+    const inputRef = useRef<HTMLInputElement>(null);
 
     const [form, setForm] = useState({
         name: '',
@@ -31,11 +34,15 @@ function MyForm({ onSubmit }: MyFormProps) {
             name: '',
             description: ''
         }); // 초기화
+        if (!inputRef.current) {
+            return;
+        }
+        inputRef.current.focus();
     }
 
     return (
         <form onSubmit={handleSubmit}>
-            <input name="name" value={name} onChange={onChange}/>
+            <input name="name" value={name} onChange={onChange} ref={inputRef}/>
             <input name="description" value={description} onChange={onChange}/>
             <button type="submit">등록</button>
         </form>
