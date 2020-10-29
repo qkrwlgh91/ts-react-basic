945c949fc8c74400aba0262d880feafeb434daeb
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-10-29 09:44:16 +0900

    added new writing input function

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,12 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { useTodosDispatch } from '../contexts/TodosContext';
 
 
 function TodoForm() {
 
     const [value, setValue] = useState('');
+    
+    // useTodosDispatch Hook을 이용하여 dispatch 함수를 받아오기
+    const dispatch = useTodosDispatch()
 
     const onSubmit = (e: React.FormEvent) => {
         e.preventDefault();
+        // dispatch를 통한 새글 등록
+        dispatch({
+            type: 'CREATE',
+            text: value
+        });
         // TODO: 새 항목 생성하기
         setValue('');
     }
